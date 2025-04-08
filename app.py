45240256bf39b945eca155a197de0d4c4e303cc5
Cauan Neves <c.neves8903@gmail.com>
2025-04-07 22:59:40 -0300

    Ajustar Velocidade

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from time import sleep
 
 # Configuração do WebDriver
 def start_driver():
@@ -29,21 +30,34 @@ def formatted_price(price):
     return float(price.replace('R$', '').strip().replace(',', '.'))
 
 # Scraping
-def scrape_price(driver, link, xpaths, site, prices_dict):
+def scrape_price(driver, link, xpaths, site, prices_dict, prices_freight, freight_actions=None):
     try:
         print(f'Acessando: {site}')
         driver.get(link)
         
         if site not in prices_dict:
             prices_dict[site] = {}
+            
+        if site not in prices_freight:
+            prices_freight[site] = {}
         
+        #Preço
         for key, xpath in xpaths.items():
             try:
                 element = wait_for_element(driver, By.XPATH, xpath).text
                 prices_dict[site][key] = formatted_price(element)
-            
             except Exception as e:
                 print(f"Erro ao buscar o preço '{key}' em {site}: {e}")
+        
+        #Frete
+        if freight_actions: #Verifica se existe e não está vazia
+            for action in freight_actions: #Executa cada ação
+                try:
+                    result = action(driver) #Retorna os valores de cada função (no caso só a última que vai ter algum valor)
+                    if isinstance(result, str):
+                        prices_freight[site]['frete'] = formatted_price(result)
+                except Exception as e:
+                    print(f"Erro ao executar ação de frete em {site}: {e}")
                 
     except Exception as e:
         print(f'Erro ao acessar o site {site}: {e}')
@@ -54,18 +68,24 @@ def main():
                 {
             'site': 'Kabum',
             'url': 'https://www.kabum.com.br/produto/627947',
-            'xpaths': {
+            'xpaths_price': {
                 'price_in_cash': '//div/h4[@class="sc-5492faee-2 ipHrwP finalPrice"]',
                 'price_full': '//div/b[@class="regularPrice"]'
-            }
+            },
+            'xpaths_freight': [
+                lambda driver: driver.find_element(By.ID, 'inputCalcularFrete').send_keys('23900650'),
+                lambda driver: driver.find_element(By.ID, 'botaoCalcularFrete').click(),
+                lambda driver: wait_for_element(driver, By.XPATH, '(//span[@class= "sc-4253a8e4-2 etvZuo"])[2]').text
+                ] 
         },
         {   
             'site': 'Terabyte',
             'url': 'https://www.terabyteshop.com.br/produto/31959/gabinete-gamer-redragon-wideload-extreme-mid-tower-rgb-vidro-curvado-temperado-atx-black-sem-fonte-sem-fan-ca-605b',
-            'xpaths': {
+            'xpaths_price': {
                 'price_in_cash': '//p[@id="valVista"]',
                 'price_full': '(//span[@id="valParc"])[2]'
-            }
+            },
+            'xpaths_freight': []
         }
     ]
 
@@ -84,8 +104,15 @@ def main():
                       ░                                                                             
                       ''')
         prices = {}
+        freight = {}
         for link in links:
-            scrape_price(driver, link['url'], link['xpaths'], link['site'], prices)
+            scrape_price(driver, link['url'], link['xpaths_price'], link['site'], prices, freight, freight_actions=link['xpaths_freight'])
+        
+        for site, price_data in prices.items():
+            print(f'{site}: {price_data}')
+            if site in freight and freight[site].get('frete'):
+                print(f"  Frete: {freight[site]['frete']}")
+
             
         for site, price_data in prices.items():
             print(f'{site}: {price_data}')
